3bbe4a0e52bbce2e4e8cc254ee11435ce12e2210
lhie1 <lhie1x@gmail.com>
2018-10-12 21:06:59 +0800

    Update app/Controllers/UserController.php

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -70,12 +70,12 @@ class UserController extends BaseController
 
         $ssr_sub_token = LinkController::GenerateSSRSubCode($this->user->id, 0);
 
-        $ss_url_0 = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_ss=1&is_mu=0');
-        $ss_url_1 = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_ss=1&is_mu=1');
-        $ss_url_0_mitm = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_ss=1&is_mu=0&mitm=1');
-        $ss_url_1_mitm = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_ss=1&is_mu=1&mitm=1');
-        $ss_url_0_mitm_new = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_ss=1&is_mu=0&mitm=1&new=1');
-        $ss_url_1_mitm_new = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_ss=1&is_mu=1&mitm=1&new=1');
+        $ss_url_0 = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_mu=0');
+        $ss_url_1 = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_mu=1');
+        $ss_url_0_mitm = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_mu=0&mitm=1');
+        $ss_url_1_mitm = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_mu=1&mitm=1');
+        $ss_url_0_mitm_new = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_mu=0&mitm=1&new=1');
+        $ss_url_1_mitm_new = urlencode(Config::get('apiUrl').'/link/'.$ios_token.'?is_mu=1&mitm=1&new=1');
 
         $ssr_url_0 = Tools::base64_url_encode(Config::get('apiUrl').'/link/'.$ssr_sub_token.'?mu=0');
         $ssr_url_1 = Tools::base64_url_encode(Config::get('apiUrl').'/link/'.$ssr_sub_token.'?mu=1');
