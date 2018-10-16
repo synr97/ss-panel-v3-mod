91c377d88c198c87b39555065f16a4399d0e1e59
lhie1 <lhie1x@gmail.com>
2018-10-16 17:11:23 +0800

    Update app/Controllers/LinkController.php

--- app/Controllers/LinkController.php
+++ app/Controllers/LinkController.php
@@ -482,9 +482,9 @@ class LinkController extends BaseController
             }
             if ($clashx == 1) {
                 if (URL::getclashXObfs($item) != "") {
-                    $proxy_list .= '- {name: '.'"'.$item['remark'].'", type: ss, server: '.$item['address'].', port: '.$item['port'].', cipher: '.$item['method'].', password: '.'"'.$item['passwd'].'",'.URL::getSurgeObfs($item).'" }\n'
+                    $proxy_list .= '- {name: '.'"'.$item['remark'].'", type: ss, server: '.$item['address'].', port: '.$item['port'].', cipher: '.$item['method'].', password: '.'"'.$item['passwd'].'",'.URL::getSurgeObfs($item).'" }\n';
                 } else {
-                    $proxy_list .= '- {name: '.'"'.$item['remark'].'", type: ss, server: '.$item['address'].', port: '.$item['port'].', cipher: '.$item['method'].', password: '.'"'.$item['passwd'].'" }\n'
+                    $proxy_list .= '- {name: '.'"'.$item['remark'].'", type: ss, server: '.$item['address'].', port: '.$item['port'].', cipher: '.$item['method'].', password: '.'"'.$item['passwd'].'" }\n';
                 }
             } else {
                 if (URL::getSurgeObfs($item) != "") {
